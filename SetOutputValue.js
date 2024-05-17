21d6a2e9359d6b6c498dfce1f5be4755a6cca803
Marcus white <me@marcus-white.com>
2024-05-17 22:10:02 +0100

    Set useful status ifnormation

--- SetOutputValue.js
+++ SetOutputValue.js
@@ -1,11 +1,38 @@
 const PiconZero = require("./piconzerojs");
 
 module.exports = function(RED){
-	function SetOutputConfig(config){
+	function SetOutputValue(config){
 		RED.nodes.createNode(this, config);
 
 		this.on("input", (msg, send, done) => {				
 			PiconZero.setOutput(parseFloat(config.outputid), parseFloat(config.value));
+
+			let configMode;
+			let configValue;
+			switch(config.value){
+				case PiconZero.ONOFF: 
+					configMode = "On/Off"; 
+					configValue = config.value === 1 ? "On" : "Off"; 
+				break;
+				case PiconZero.PWM: 
+					configMode = "PWM"; 
+					configValue = config.value + "%"; 
+				break;
+				case PiconZero.SERVO: 
+					configMode = "Servo"; 
+					configValue = config.value + "deg"
+				break;
+				case PiconZero.WS2812B: 
+					configMode = "WS2812B"; 
+					configValue = config.value
+				break;
+			}
+
+			scope.status({
+				fill: "blue",
+				shape: "ring",
+				text: `${configMode} output ${config.outputid} value is ${configValue}`
+			});
 
 			if(done){
 				done();
@@ -13,5 +40,5 @@ module.exports = function(RED){
 		});
 
 	}
-	RED.nodes.registerType("Set Output Value", SetOutputConfig)
+	RED.nodes.registerType("Set Output Value", SetOutputValue)
 }
