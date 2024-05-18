07b4ffa1f7044e89b8fa3be08665204526df1333
Marcus white <me@marcus-white.com>
2024-05-18 22:10:46 +0100

    Make Motor node parse inputs nicely and msgs

--- SetMotorNode.js
+++ SetMotorNode.js
@@ -1,11 +1,26 @@
 const PiconZero = require("./piconzerojs");
+const util = require("./util");
+
 
 module.exports = function(RED){
 	function SetMotorNode(config){
 		RED.nodes.createNode(this, config);
 
 		this.on("input", (msg, send, done) => {				
-			PiconZero.setMotor(parseInt(config.motorid), parseFloat(config.value));
+
+			util.checkIsInitialised();
+
+			const motorId = parseInt(RED.util.evaluateNodeProperty(msg.payload?.motorid, "msg", this, msg) || config.motorid);
+			if(motorId === NaN){
+				throw new Error("'motorId' not found in payload or node config");
+			}
+
+			const value = parseInt(RED.util.evaluateNodeProperty(msg.payload?.value, "msg", this, msg) || config.value);
+			if(value === NaN){
+				throw new Error("'value' not found in payload or node config.")
+			}
+
+			PiconZero.setMotor(parseInt(motorId), parseFloat(value));
 
 			send(msg);
 			if(done){
