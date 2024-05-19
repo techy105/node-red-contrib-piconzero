dc9efad26c45ae5f2e267d31a728a97ecf8d24fe
Marcus white <me@marcus-white.com>
2024-05-20 00:28:28 +0100

    add Set Input Mode

---
+++ nodes/SetInputMode/SetInputMode.js
@@ -0,0 +1,56 @@
+const PiconZero = require("../../piconzerojs");
+const util = require("../../lib/util");
+
+
+module.exports = function(RED){
+	function SetInputMode(config){
+		RED.nodes.createNode(this, config);
+		this.on("input", function(msg, send, done) {	
+			util.checkIsInitialised(this);
+
+
+			const inputId = parseInt(msg.payload?.input?.id || config.inputid);
+			if(inputId === NaN){
+				throw new Error("'input[id]' not found in payload or node config");
+			}
+
+			const mode = parseInt(msg.payload?.input?.mode || config.mode);
+			if(mode === NaN){
+				throw new Error("'input[mode]' not found in payload or node config.")
+			}
+
+			let pullup = msg.payload?.input?.pullup || config.pullup;
+			if(pullup !== true && pullup !== false){
+				pullup = false;
+			}
+
+			let period = parseInt(msg.payload?.input?.period || config.period);
+			if(period === NaN){
+				period = 2000;
+			}
+
+			const flow = this.context().flow;
+			
+			PiconZero.setInputConfig(inputId, mode, pullup, period);
+
+			const inputType = flow.get(util.GLOBAL_PREFIX + "Input" + inputId + "Config");		
+			const configModeValues = util.getConfigModeValue(inputType);
+
+			this.status({
+				fill: "blue",
+				shape: "ring",
+				text: `Input ${inputId} mode is now ${configModeValues}`
+			});
+
+			send(msg);
+
+			if(done){
+				done();
+			}
+
+			return msg;
+		});
+
+	}
+	RED.nodes.registerType("Set Input Mode", SetInputMode)
+}
